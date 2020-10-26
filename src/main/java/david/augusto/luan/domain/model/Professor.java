175f2a04f41bc7104b301c9d1234c0dc4253d459
luan-alencar <luanalencar134@gmail.com>
2020-10-26 17:26:33 -0300

    implementando no pacote domain algumas classes entidades

---
+++ src/main/java/david/augusto/luan/domain/model/Professor.java
@@ -0,0 +1,92 @@
+package david.augusto.luan.domain.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+public class Professor {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank
+	private String nome;
+
+	@NotBlank
+	private Double salario;
+
+	@NotBlank
+	private String titulacao;
+
+	public Professor(Long id, @NotBlank String nome, @NotBlank Double salario, @NotBlank String titulacao) {
+		this.id = id;
+		this.nome = nome;
+		this.salario = salario;
+		this.titulacao = titulacao;
+	}
+
+	public Professor() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(Double salario) {
+		this.salario = salario;
+	}
+
+	public String getTitulacao() {
+		return titulacao;
+	}
+
+	public void setTitulacao(String titulacao) {
+		this.titulacao = titulacao;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Professor other = (Professor) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
